4768dc772867842446758e18694791ba065b70ab
Cristian <cryssthrill@gmail.com>
2026-01-15 18:23:01 +0200

    da ba

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.jssssss and Supabase Starter Kit",
+  title: "Next.jssssss test and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
